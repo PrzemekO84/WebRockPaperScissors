8e69e8247d52642a13ba46045b4691790c25e1d3
PrzemekO84 <przemekorzechowski53@gmail.com>
2024-08-16 21:30:52 +0200

    Ended the layout and started on some basic logic :)

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,6 +1,5 @@
 
 
-
 function hideRules() {
     const rulesSection = document.getElementById("rules");
     const gameSection = document.getElementById("game");
@@ -10,5 +9,55 @@ function hideRules() {
     gameSection.style.display = "block"
     headerDiv.style.display = "none"
     
+}
+
+function Game() {
+
+
+    const rock = document.getElementById("rock");
+    const paper = document.getElementById("paper");
+    const scissors = document.getElementById("scissors");
+    const playerScore = document.getElementById("playerScore");
+    const computerScore = document.getElementById("computerScore");
+
+}
+
+function Move(move) {
+
+    let playerMove;
+    let computerMove;
+    let computerRandomNumber
+
+    // PlayerMove
+
+    if (move.id === rock.id){
+        playerMove = "rock"
+    }
+    else if (move.id === paper.id){
+        playerMove = "paper"
+    }
+    else{
+        playerMove = "scissors"
+    }
+
+    //ComputerMove
+
+    computerRandomNumber = Math.floor(Math.random() * 3)
+
+    if (computerRandomNumber === 0){
+        computerMove = "rock";
+    }
+    else if (computerRandomNumber === 1){
+        computerMove = "paper";
+    }
+    else{
+        computerMove = "scissors";
+    }
+
+
+    console.log(`Player move is ${playerMove}`);
+    console.log(`Computer move is ${computerMove}`);
+
+    return {playerMove, computerMove}
 }
 
