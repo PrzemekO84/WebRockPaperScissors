25e4768b227aeb5f8b3733b4890d54223825eb1b
PrzemekO84 <przemekorzechowski53@gmail.com>
2024-08-17 22:34:21 +0200

    Added game logic and scoreboard update

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,5 +1,8 @@
 
 
+let playerScoreNumber = 0;
+let computerScoreNumer = 0;
+
 function hideRules() {
     const rulesSection = document.getElementById("rules");
     const gameSection = document.getElementById("game");
@@ -11,15 +14,69 @@ function hideRules() {
     
 }
 
-function Game() {
-
-
+function Game(playerMove, computerMove) {
+    
     const rock = document.getElementById("rock");
     const paper = document.getElementById("paper");
     const scissors = document.getElementById("scissors");
-    const playerScore = document.getElementById("playerScore");
-    const computerScore = document.getElementById("computerScore");
+    const playerScoreText = document.getElementById("playerScore");
+    const computerScoreText = document.getElementById("computerScore");
 
+    console.log(`Player move is ${playerMove}`);
+    console.log(`Computer move is ${computerMove}`);
+
+    // Game Logic
+
+    switch(playerMove){
+        case "rock":
+            if(computerMove === "paper"){
+                console.log("computer choosed paper = paper wins with rock");
+                console.log("Computer wins");
+                computerScoreNumer++;
+            }
+            else if(computerMove === "scissors"){
+                console.log("computer choosed scissors = scissors lose with rock");
+                console.log("Player wins");
+                playerScoreNumber++;
+            }
+            else{
+                console.log("draw");
+            }
+            break;
+        case "paper":
+            if(computerMove === "rock"){
+                console.log("computer choosed rock = paper wins with rock");
+                console.log("Player wins");
+                playerScoreNumber++;
+            }
+            else if(computerMove === "scissors"){
+                console.log("computer choosed scissors = scissors wins with paper");
+                console.log("Computer wins");
+                computerScoreNumer++;
+            }
+            else{
+                console.log("draw");
+            }
+            break;
+        case "scissors":
+            if(computerMove === "rock"){
+                console.log("computer choosed rock = rock wins with scissors");
+                console.log("Computer wins");
+                computerScoreNumer++;
+            }
+            else if(computerMove === "paper"){
+                console.log("computer choosed paper = scissors wins with paper");
+                console.log("Player wins");
+                playerScoreNumber++;
+            }
+            else{
+                console.log("draw");
+            }
+            break;
+    }
+
+    playerScoreText.textContent = `Player score: ${playerScoreNumber}`;
+    computerScoreText.textContent = `Computer score: ${computerScoreNumer}`;
 }
 
 function Move(move) {
@@ -54,10 +111,6 @@ function Move(move) {
         computerMove = "scissors";
     }
 
-
-    console.log(`Player move is ${playerMove}`);
-    console.log(`Computer move is ${computerMove}`);
-
-    return {playerMove, computerMove}
+    Game(playerMove, computerMove)
 }
 
