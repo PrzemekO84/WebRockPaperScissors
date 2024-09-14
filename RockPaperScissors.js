cf7ad86b88c7636fbdaf6c04a7001187611b128f
PrzemekO84 <przemekorzechowski53@gmail.com>
2024-09-14 14:40:07 +0200

    On the finish of a game counter

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,7 +1,7 @@
 
 //Score
 let playerScoreNumber = 0;
-let computerScoreNumer = 0;
+let computerScoreNumber = 0;
 
 function hideRules() {
 
@@ -22,8 +22,6 @@ function Game(playerMove, computerMove) {
     const rock = document.getElementById("rock");
     const paper = document.getElementById("paper");
     const scissors = document.getElementById("scissors");
-    const playerScoreText = document.getElementById("playerScore");
-    const computerScoreText = document.getElementById("computerScore");
 
     console.log(`Player move is ${playerMove}`);
     console.log(`Computer move is ${computerMove}`);
@@ -35,7 +33,7 @@ function Game(playerMove, computerMove) {
             if(computerMove === "paper"){
                 console.log("computer choosed paper = paper wins with rock");
                 console.log("Computer wins");
-                computerScoreNumer++;
+                computerScoreNumber++;
             }
             else if(computerMove === "scissors"){
                 console.log("computer choosed scissors = scissors lose with rock");
@@ -55,7 +53,7 @@ function Game(playerMove, computerMove) {
             else if(computerMove === "scissors"){
                 console.log("computer choosed scissors = scissors wins with paper");
                 console.log("Computer wins");
-                computerScoreNumer++;
+                computerScoreNumber++;
             }
             else{
                 console.log("draw");
@@ -65,7 +63,7 @@ function Game(playerMove, computerMove) {
             if(computerMove === "rock"){
                 console.log("computer choosed rock = rock wins with scissors");
                 console.log("Computer wins");
-                computerScoreNumer++;
+                computerScoreNumber++;
             }
             else if(computerMove === "paper"){
                 console.log("computer choosed paper = scissors wins with paper");
@@ -78,11 +76,7 @@ function Game(playerMove, computerMove) {
             break;
     }
 
-    playerScoreText.textContent = `Player score: ${playerScoreNumber}`;
-    computerScoreText.textContent = `Computer score: ${computerScoreNumer}`;
-
-    gameCount(playerMove, computerMove);
-    settingImage(playerMove, computerMove);
+    Continue(playerMove, computerMove);
 }
 
 function Move(move) {
@@ -129,46 +123,54 @@ function gameCount(playerMove, computerMove){
     const gameCount = document.getElementById("gameCount");
     const noneDiv = document.getElementsByClassName("noneDiv");
 
-    //Creating Counter elements
-    const counterDiv = document.createElement("div");
-    const counterH1 = document.createElement("h1");
+    return new Promise((resolve) => {
+        //Creating Counter elements
+        const counterDiv = document.createElement("div");
+        const counterH1 = document.createElement("h1");
 
-    counterDiv.id = "counterDiv";
-    counterH1.id = "counterH1";
+        counterDiv.id = "counterDiv";
+        counterH1.id = "counterH1";
+
+        counterDiv.className = "counterDiv";
+        counterH1.className = "counterH1";
+
+
+        //Hiding mainMoveBox
+        mainMoveBox.style.display = "none";
+
+        gameCount.style.display = "block"
+
+        //Adding counter to the document
+        document.getElementById("gameCount").append(counterDiv);
+        document.getElementById("counterDiv").append(counterH1);
+
+        console.log(counterH1.textContent);
+
+        counterH1.textContent = "ROCK! 🪨";
+
+        let countingInterval = setInterval(() => {
+            if(counterH1.textContent === "ROCK! 🪨"){
+                countText = "PAPER! 📄";
+            }       
+            else if(counterH1.textContent === "PAPER! 📄"){
+                countText = "SCISSORS! ✂️";
+
+                setTimeout(() => {
+                    counterDiv.style.display = "none";
+                    for(let index = 0; index < noneDiv.length; index++){
+                        noneDiv[index].style.display = "flex";  
+                    }
+                }, 1000);
+
+                clearInterval(countingInterval);
+                resolve();
+            }
+            counterH1.textContent = countText;
     
-
-    //Hiding mainMoveBox
-    mainMoveBox.style.display = "none";
-
-    gameCount.style.display = "block"
-
-    //Adding counter to the document
-    document.getElementById("gameCount").append(counterDiv);
-    document.getElementById("counterDiv").append(counterH1);
-
-    console.log(counterH1.textContent);
-    
-    counterH1.textContent = "ROCK! 🪨";
-
-    let countingInterval = setInterval(() => {
-        if(counterH1.textContent === "ROCK! 🪨"){
-            countText = "PAPER! 📄";
-        }
-        else if(counterH1.textContent === "PAPER! 📄"){
-            countText = "SCISSORS! ✂️";
-
-            setTimeout(() => {
-                counterDiv.style.display = "none";
-                for(let index = 0; index < noneDiv.length; index++){
-                    noneDiv[index].style.display = "flex";  
-                }
-            }, 1000);
-
-            clearInterval(countingInterval);
-        }
-        counterH1.textContent = countText;
+        }, 1000);
         
-    }, 1000);
+    })
+
 
 }
 
@@ -180,36 +182,94 @@ function settingImage(playerMove, computerMove){
     const playerMoveText = document.getElementById("playerMove");
     const computerMoveText = document.getElementById("computerMove");
 
-    switch(playerMove){
-        case "rock":
-            playerMovePicture.src = "Images/rockImage.png";
-            playerMoveText.textContent = "Rock"
-            break;
-        case "paper":
-            playerMovePicture.src = "Images/paperImage.png";
-            playerMoveText.textContent = "Paper"
-            break;
-        case "scissors":
-            playerMovePicture.src = "Images/scissorsImage.png";
-            playerMoveText.textContent = "Scissors"
-            break;
-        
-    }
+    playerMoveText.style.fontSize = "36px"
+    computerMoveText.style.fontSize = "36px"
 
-    switch(computerMove){
-        case "rock":
-            computerMovePicture.src = "Images/rockImage.png";
-            computerMoveText.textContent = "Rock"
-            break;
-        case "paper":
-            computerMovePicture.src = "Images/paperImage.png";
-            computerMoveText.textContent = "Paper"
-            break;
-        case "scissors":
-            computerMovePicture.src = "Images/scissorsImage.png";
-            computerMoveText.textConent = "Scissors" 
-            break;
-    }
+
+    return new Promise((resolve) => {
+        console.log("Setting images...");
+        setTimeout(() => {
+            switch(playerMove){
+                case "rock":
+                    playerMovePicture.src = "Images/rockImage.png";
+                    playerMoveText.textContent = "Player: Rock"
+                    break;
+                case "paper":
+                    playerMovePicture.src = "Images/paperImage.png";
+                    playerMoveText.textContent = "Player: Paper"
+                    break;
+                case "scissors":
+                    playerMovePicture.src = "Images/scissorsImage.png";
+                    playerMoveText.textContent = "Player: Scissors"
+                    break;
+                
+            }
+        
+            switch(computerMove){
+                case "rock":
+                    computerMovePicture.src = "Images/rockImage.png";
+                    computerMoveText.textContent = "Computer: Rock"
+                    break;
+                case "paper":
+                    computerMovePicture.src = "Images/paperImage.png";
+                    computerMoveText.textContent = "Computer: Paper"
+                    break;
+                case "scissors":
+                    computerMovePicture.src = "Images/scissorsImage.png";
+                    computerMoveText.textContent = "Computer: Scissors" 
+                    break;
+            }
+            console.log("Images set, resolving...");
+            resolve();
+        }, 500);
+        
+    })
+}
+
+async function Continue(playerMove, computerMove){
+
+    const noneDiv = document.getElementsByClassName("noneDiv");
+    const gameCountDiv = document.getElementById("gameCount");
+    
+    await gameCount(playerMove, computerMove);
+    
+    await settingImage(playerMove, computerMove);
+
+    console.log("Images set and game count done");
+
+    console.log("Starting 2 sec counter");
+    await new Promise((resolve) => {
+        setTimeout(() => {
+
+            const playerScoreText = document.getElementById("playerScore");
+            const computerScoreText = document.getElementById("computerScore");
+
+            for(let index = 0; index < noneDiv.length; index++){
+                noneDiv[index].style.display = "none";  
+            }
+
+            playerScoreText.textContent = `Player score: ${playerScoreNumber}`;
+            computerScoreText.textContent = `Computer score: ${computerScoreNumber}`;
+
+            console.log("2seconds by");
+            resolve();
+        }, 4000);
+    });
+
+    const continueDiv = document.createElement("div");
+    const continueH1 = document.createElement("h1");
+
+    continueDiv.id = "continueDiv";
+    continueH1.id = "continueH1";
+
+    continueDiv.className = "counterDiv";
+    continueH1.className = "counterH1";
+
+    document.getElementById("gameCount").append(continueDiv);
+    document.getElementById("continueDiv").append(continueH1);
+
+    continueH1.textContent = "Siema!"
 
 }
+
 
